fd0a5f94f5e38caaedf385b5ba13aa5c9536cb70
KyleHuang-Dev <kyle.huang.dev@gmail.com>
2023-02-24 15:32:25 -0700

    Clean up components

--- e-commerce/packages/web/pages/user/my-profile.tsx
+++ e-commerce/packages/web/pages/user/my-profile.tsx
@@ -1,10 +1,11 @@
 import OrderList from "@/src/components/OrderList";
 import { Order } from "@/src/store/cart/cart.model";
 import { useUser } from "@/src/store/user/user.hook";
+import ProfileBody from "@/src/views/ProfileBody";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-interface IUser {
+export interface IUser {
     id: number;
     email: string;
     hash: string;
@@ -23,34 +24,23 @@ export default function MyProfile() {
     const bearer = `Bearer ${token}`;
     useEffect(() => {
         async function getUser() {
-            const res = await axios.get(
-                //put it in env
-                `http://localhost:2121/user/my-profile`,
-                {
-                    headers: { Authorization: bearer },
-                }
-            );
-            console.log(res.data);
-            setUser(res.data);
+            try {
+                const res = await axios.get(
+                    //put it in env
+                    `http://localhost:2121/user/my-profile`,
+                    {
+                        headers: { Authorization: bearer },
+                    }
+                );
+                console.log(res.data);
+                setUser(res.data);
+            } catch (error) {
+                console.log(error);
+            }
         }
 
         getUser();
     }, []);
 
-    const { userName, email, orders, isAdmin } = user;
-
-    return !currentUser ? (
-        <h1>login fist</h1>
-    ) : (
-        <>
-            <h1>Hello {userName}</h1>
-            <h1>{email}</h1>
-            {isAdmin ? <a href="/admin">Admin</a> : <></>}
-            {!orders
-                ? null
-                : orders.map((order) => (
-                      <OrderList props={order} key={order.id} />
-                  ))}
-        </>
-    );
+    return !currentUser ? <h1>login fist</h1> : <ProfileBody user={user} />;
 }
